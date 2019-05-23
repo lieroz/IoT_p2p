8d8b42e558a8b9590ccc9ee266d5a5623a98047b
Andrey Kamakin <a.kamakin@icloud.com>
2019-05-23 21:56:21 +0100

    Changed RHGenericSPI

--- lora/RHGenericSPI.cpp
+++ lora/RHGenericSPI.cpp
@@ -1,9 +1,3 @@
-// RHGenericSPI.cpp
-// Author: Mike McCauley (mikem@airspayce.com)
-// Copyright (C) 2011 Mike McCauley
-// Contributed by Joanna Rutkowska
-// $Id: RHGenericSPI.cpp,v 1.2 2014/04/12 05:26:05 mikem Exp $
-
 #include "RHGenericSPI.h"
 
 RHGenericSPI::RHGenericSPI(Frequency frequency, BitOrder bitOrder, DataMode dataMode)
