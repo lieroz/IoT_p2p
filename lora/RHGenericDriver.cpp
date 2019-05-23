504c44a37a6e3fede544c106a49d1f3705809c00
Andrey Kamakin <a.kamakin@icloud.com>
2019-05-23 21:11:22 +0100

    Changed RHGenericDriver

--- lora/RHGenericDriver.cpp
+++ lora/RHGenericDriver.cpp
@@ -1,8 +1,3 @@
-// RHGenericDriver.cpp
-//
-// Copyright (C) 2014 Mike McCauley
-// $Id: RHGenericDriver.cpp,v 1.20 2017/01/12 23:58:00 mikem Exp $
-
 #include "RHGenericDriver.h"
 
 RHGenericDriver::RHGenericDriver()
@@ -25,26 +20,24 @@ bool RHGenericDriver::init()
     return true;
 }
 
-// Blocks until a valid message is received
 void RHGenericDriver::waitAvailable()
 {
     while (!available())
-	YIELD;
+    {
+	    YIELD;
+    }
 }
 
-// Blocks until a valid message is received or timeout expires
-// Return true if there is a message available
-// Works correctly even on millis() rollover
 bool RHGenericDriver::waitAvailableTimeout(uint16_t timeout)
 {
     unsigned long starttime = millis();
     while ((millis() - starttime) < timeout)
     {
         if (available())
-	{
-           return true;
-	}
-	YIELD;
+        {
+               return true;
+        }
+        YIELD;
     }
     return false;
 }
@@ -52,7 +45,9 @@ bool RHGenericDriver::waitAvailableTimeout(uint16_t timeout)
 bool RHGenericDriver::waitPacketSent()
 {
     while (_mode == RHModeTx)
-	YIELD; // Wait for any previous transmit to finish
+    {
+	    YIELD;
+    }
     return true;
 }
 
@@ -61,36 +56,34 @@ bool RHGenericDriver::waitPacketSent(uint16_t timeout)
     unsigned long starttime = millis();
     while ((millis() - starttime) < timeout)
     {
-        if (_mode != RHModeTx) // Any previous transmit finished?
-           return true;
-	YIELD;
+        if (_mode != RHModeTx)
+        {
+            return true;
+        }
+	    YIELD;
     }
     return false;
 }
 
-// Wait until no channel activity detected or timeout
 bool RHGenericDriver::waitCAD()
 {
     if (!_cad_timeout)
-	return true;
+    {
+	    return true;
+    }
 
-    // Wait for any channel activity to finish or timeout
-    // Sophisticated DCF function...
-    // DCF : BackoffTime = random() x aSlotTime
-    // 100 - 1000 ms
-    // 10 sec timeout
     unsigned long t = millis();
     while (isChannelActive())
     {
          if (millis() - t > _cad_timeout) 
-	     return false;
-         //delay(random(1, 10) * 100); // Should these values be configurable? Macros?
+         {
+	        return false;
+         }
     }
 
     return true;
 }
 
-// subclasses are expected to override if CAD is available for that radio
 bool RHGenericDriver::isChannelActive()
 {
     return false;
@@ -172,20 +165,18 @@ void RHGenericDriver::printBuffer(const char* prompt, const uint8_t* buf, uint8_
 {
     uint8_t i;
 
-#ifdef RH_HAVE_SERIAL
     Serial.println(prompt);
     for (i = 0; i < len; i++)
     {
-	if (i % 16 == 15)
-	    Serial.println(buf[i], HEX);
-	else
-	{
-	    Serial.print(buf[i], HEX);
-	    Serial.print(' ');
-	}
+        if (i % 16 == 15)
+            Serial.println(buf[i], HEX);
+        else
+        {
+            Serial.print(buf[i], HEX);
+            Serial.print(' ');
+        }
     }
     Serial.println("");
-#endif
 }
 
 uint16_t RHGenericDriver::rxBad()
@@ -208,11 +199,3 @@ void RHGenericDriver::setCADTimeout(unsigned long cad_timeout)
     _cad_timeout = cad_timeout;
 }
 
-#if (RH_PLATFORM == RH_PLATFORM_ARDUINO) && defined(RH_PLATFORM_ATTINY)
-// Tinycore does not have __cxa_pure_virtual, so without this we
-// get linking complaints from the default code generated for pure virtual functions
-extern "C" void __cxa_pure_virtual()
-{
-    while (1);
-}
-#endif
