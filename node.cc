acb063db72fd628fca04ef7b8164e92a30c38856
Andrey Kamakin <a.kamakin@icloud.com>
2019-06-04 13:02:17 +0100

    fixed memory errors

--- node.cc
+++ node.cc
@@ -5,41 +5,85 @@
 #include <thread>
 #include <cstring>
 #include <iostream>
+#include <chrono>
+
+namespace
+{
+std::chrono::time_point<std::chrono::high_resolution_clock> timePoint;
+
+void txCallback(txData *tx)
+{
+    std::cout << "tx done;\tsent string: " << tx->buf << std::endl;
+
+    LoRa_ctl *modem = (LoRa_ctl *)(tx->userPtr);
+    std::memset(modem->rx.data.buf, '\0', Node::loraBufSize);
+    LoRa_receive(modem);
+}
+
+void rxCallback(rxData *rx)
+{
+    timePoint = std::chrono::high_resolution_clock::now();
+
+    LoRa_ctl *modem = (LoRa_ctl *)(rx->userPtr);
+    LoRa_stop_receive(modem);
+
+    std::cout << "rx done;\tCRC error: " << rx->CRC
+              << "\tData size: " << rx->size
+              << "\tReceived string: \"" << rx->buf
+              << "\";\tRSSI: " << rx->RSSI
+              << ";\tSNR: " << rx->SNR << std::endl;
+
+    std::memset(modem->tx.data.buf, '\0', Node::loraBufSize);
+    std::memcpy(modem->tx.data.buf, "pong", 4);
+    modem->tx.data.size = 4;
+
+    LoRa_send(modem);
+}
+
+}
 
 Node::Node(std::size_t _timeout)
     : keys(tools::RsaGenerateHexKeyPair(rsaBits)),
     timeout(_timeout)
 {
-    init();
 }
 
-void Node::init()
+void Node::init(LoRa_ctl *modem, char *txbuf, char *rxbuf)
 {
-    modem.spiCS = 0; //Raspberry SPI CE pin number
-    modem.tx.callback = (UserTxDoneCallback)&Node::txCallback;
-    modem.tx.data.buf = txbuf;
-    modem.rx.callback = (UserRxDoneCallback)&Node::rxCallback;
-    modem.rx.data.buf = rxbuf;
-    modem.rx.data.userPtr = static_cast<void *>(&modem); //To handle with chip from rx callback
-    modem.tx.data.userPtr = static_cast<void *>(&modem); //To handle with chip from tx callback
-    modem.eth.preambleLen = 6;
-    modem.eth.bw = BW62_5; //Bandwidth 62.5KHz
-    modem.eth.sf = SF12; //Spreading Factor 12
-    modem.eth.ecr = CR8; //Error coding rate CR4/8
-    modem.eth.CRC = 1; //Turn on CRC checking
-    modem.eth.freq = 434800000; // 434.8MHz
-    modem.eth.resetGpioN = 4; //GPIO4 on lora RESET pin
-    modem.eth.dio0GpioN = 17; //GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
-    modem.eth.outPower = OP20; //Output power
-    modem.eth.powerOutPin = PA_BOOST; //Power Amplifire pin
-    modem.eth.AGC = 1; //Auto Gain Control
-    modem.eth.OCP = 240; // 45 to 240 mA. 0 to turn off protection
-    modem.eth.implicitHeader = 0; //Explicit header mode
-    modem.eth.syncWord = 0x12;
+    modem->spiCS = 0; //Raspberry SPI CE pin number
+    modem->tx.callback = txCallback;
+    modem->tx.data.buf = txbuf;
+    modem->rx.callback = rxCallback;
+    modem->rx.data.buf = rxbuf;
+    modem->rx.data.userPtr = static_cast<void *>(modem);
+    modem->tx.data.userPtr = static_cast<void *>(modem);
+    modem->eth.preambleLen = 6;
+    modem->eth.bw = BW62_5; //Bandwidth 62.5KHz
+    modem->eth.sf = SF12; //Spreading Factor 12
+    modem->eth.ecr = CR8; //Error coding rate CR4/8
+    modem->eth.CRC = 1; //Turn on CRC checking
+    modem->eth.freq = 434800000; // 434.8MHz
+    modem->eth.resetGpioN = 4; //GPIO4 on lora RESET pin
+    modem->eth.dio0GpioN = 17; //GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
+    modem->eth.outPower = OP20; //Output power
+    modem->eth.powerOutPin = PA_BOOST; //Power Amplifire pin
+    modem->eth.AGC = 1; //Auto Gain Control
+    modem->eth.OCP = 240; // 45 to 240 mA. 0 to turn off protection
+    modem->eth.implicitHeader = 0; //Explicit header mode
+    modem->eth.syncWord = 0x12;
 }
 
 void Node::start()
 {
+    LoRa_ctl modem;
+    char txbuf[loraBufSize];
+    char rxbuf[loraBufSize];
+
+    std::memset(txbuf, '\0', loraBufSize);
+    std::memset(rxbuf, '\0', loraBufSize);
+
+    init(&modem, txbuf, rxbuf);
+
     if (LoRa_begin(&modem) != 0)
     {
         throw std::runtime_error("Error configuring LoRa");
@@ -51,42 +95,20 @@ void Node::start()
     do
     {
         if (std::chrono::duration_cast<std::chrono::milliseconds>(
-                    now - timePoint).count() > timeout)
+                    now - timePoint).count() >= timeout)
         {
-            std::memset(txbuf, '\0', 255);
-            std::memcpy(txbuf, "ping", 4);
+            std::memset(modem.tx.data.buf, '\0', loraBufSize);
+            std::memcpy(modem.tx.data.buf, "ping", 4);
             modem.tx.data.size = 4;
 
             LoRa_send(&modem);
             timePoint = std::chrono::high_resolution_clock::now();
         }
 
-        now = timePoint + std::chrono::milliseconds(timeout);
+        now = std::chrono::high_resolution_clock::now();
         std::this_thread::yield();
     } while (LoRa_get_op_mode(&modem) != SLEEP_MODE);
 
     LoRa_end(&modem);
-}
-
-void Node::txCallback(txData *tx)
-{
-    std::cout << "tx done;\tsent string: " << txbuf <<std::endl;
-
-    std::memset(rxbuf, '\0', 255);
-
-    LoRa_receive(&modem);
-}
-
-void Node::rxCallback(rxData *rx)
-{
-    timePoint = std::chrono::high_resolution_clock::now();
-
-    std::cout << "RX done;\treceived string: " << rxbuf << std::endl;
-
-    std::memset(txbuf, '\0', 255);
-    std::memcpy(txbuf, "pong", 4);
-    modem.tx.data.size = 4;
-
-    LoRa_send(&modem);
 }
 
--- node.h
+++ node.h
@@ -1,33 +1,24 @@
 #pragma once
 
-#include <chrono>
-
-#include <lora.h>
 #include <tools.h>
+#include <lora.h>
 
 class Node
 {
-    static constexpr std::size_t loraBufSize = 255;
-    static constexpr std::size_t rsaBits = 1040;
-
 public:
     Node(std::size_t _timeout);
 
     void start();
 
 private:
-    void init();
+    void init(LoRa_ctl *modem, char *txbuf, char *rxbuf);
 
-    void txCallback(txData *tx);
-    void rxCallback(rxData *rx);
+public:
+    static constexpr std::size_t loraBufSize = 255;
+    static constexpr std::size_t rsaBits = 1040;
 
 private:
-    LoRa_ctl modem;
-    char txbuf[loraBufSize];
-    char rxbuf[loraBufSize];
-
     tools::KeyPairHex keys;
-    std::chrono::time_point<std::chrono::high_resolution_clock> timePoint;
     std::size_t timeout;
 };
 
